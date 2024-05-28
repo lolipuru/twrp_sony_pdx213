45c1c5b80b94fe8eb0aecd4b11798fc55d86db50
lolipuru <banstellazz@gmail.com>
2024-05-29 03:23:17 +0700

    pdx213: import prebuilt kernel and modules from stock 62.2.A.0.533
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -6,6 +6,11 @@ include $(CLEAR_VARS)
     LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
     LOCAL_HOST_REQUIRED_MODULES := depmod
     LOCAL_POST_INSTALL_CMD += \
+        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules; \
+        cp -rf $(LOCAL_PATH)/$(PRODUCT_RELEASE_NAME)/modules $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/; \
+        cp -rf $(LOCAL_PATH)/modules.load.recovery $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/1.1/; \
         mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware; \
-        cp -rf $(LOCAL_PATH)/$(PRODUCT_RELEASE_NAME)/firmware $(TARGET_RECOVERY_ROOT_OUT)/vendor/; 
+        cp -rf $(LOCAL_PATH)/$(PRODUCT_RELEASE_NAME)/firmware $(TARGET_RECOVERY_ROOT_OUT)/vendor/; \
+        echo "calling depmod on sony_firmware"; \
+        $(DEPMOD) -b $(TARGET_RECOVERY_ROOT_OUT)/vendor 1.1;
 include $(BUILD_PHONY_PACKAGE)
